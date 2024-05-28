3a81b08a88f4bd32258141a2dd4adb2188a74552
ZarakiDS <145158819+ZarakiDS@users.noreply.github.com>
2024-05-28 17:18:29 +0200

    Update and rename run.bat to compile.bat

---
+++ compile.bat
@@ -0,0 +1,1 @@
+g++ *.cpp -DSFML_STATIC -IC:\SFML_MINGW\include -I./include -LC:\SFML_MINGW\lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lfreetype -o debug/TextEditor -std=c++20 -Wall -O3 -mwindows
--- run.bat
+++
@@ -1,1 +0,0 @@
-g++ *.cpp -DSFML_STATIC -IC:\SFML_MINGW\include -I./include -LC:\SFML_MINGW\lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lfreetype -o debug/TextEditor -std=c++20 -Wall -O3 -mwindows 
