c8efd5fcc2a297e7c0350a7d9a64dd5c1c485d71
ZarakiDS <145158819+ZarakiDS@users.noreply.github.com>
2024-05-28 17:17:25 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,1 @@
-g++ *.cpp -DSFML_STATIC -IC:\SFML_MINGW\include -I./include -LC:\SFML_MINGW\lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lfreetype -o debug/main -std=c++20 -Wall -O3 res/my.res 
-rem -mwindows
-debug\main 
+g++ *.cpp -DSFML_STATIC -IC:\SFML_MINGW\include -I./include -LC:\SFML_MINGW\lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32 -lfreetype -o debug/TextEditor -std=c++20 -Wall -O3 -mwindows 
